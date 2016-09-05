08cdf4d3743753ae02aa57a26773407ace65f052
wangsheng666 <1028742881@qq.com>
2016-09-05 08:52:07 +0800

    MainActivity.java  have a Button ,press it can start the AnotherActivity. then the AnotherActivity have a Button_return,when press it can start the MainActivity

---
+++ test9/src/main/java/com/example/test9/AnotherActivity.java
@@ -0,0 +1,29 @@
+package com.example.test9;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_another);
+        Button button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AnotherActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+}
